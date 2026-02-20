32f0f663c0665a29868852ec7c3100fbe22ea54a
PresidentTree94 <karlysams1218@gmail.com>
2026-02-19 20:22:03 -0500

    Implement edit myth characters.

--- components/MyCh.tsx
+++ components/MyCh.tsx
@@ -2,10 +2,14 @@
 import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { MyChar } from "../types/myChar";
+import Modal from "./Modal";
 
 export default function MyCh({ data }: { data: MyChar }) {
 
+  const [open, setOpen] = useState(false);
   const [inspiration, setInspiration] = useState("");
+  const [contribution, setContribution] = useState(data.contribution);
+
   useEffect(() => {
     const fetchData = async () => {
       const { data: character } = await supabase.from("characters").select("*").eq("id", data.character_id).single();
@@ -14,13 +18,45 @@ export default function MyCh({ data }: { data: MyChar }) {
     fetchData();
   }, [data.character_id]);
 
+  const elements = {
+    inspiration: {
+      label: "Inspiration",
+      value: inspiration,
+      setValue: setInspiration
+    },
+    contribution: {
+      label: "Contribution",
+      value: contribution,
+      setValue: setContribution
+    }
+  }
+
+  const handleSubmit: React.SubmitEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await supabase.from("characters").update({ inspiration: inspiration.trim() }).eq("id", data.character_id);
+    await supabase.from("myth_chars").update({ contribution: contribution.trim() }).eq("myth_id", data.myth_id).eq("character_id", data.character_id);
+    setInspiration("");
+    setContribution("");
+    setOpen(false);
+  }
+
   return (
-    <div className="card p-0 overflow-hidden">
-      <div className="h-2 w-full bg-gradient-to-r from-primary via-secondary to-primary"></div>
-      <div className="p-6">
-        <h3>{inspiration}</h3>
-        <p className="font-serif">{data.contribution}</p>
+    <>
+      <div className="card p-0 overflow-hidden">
+        <div className="h-2 w-full bg-gradient-to-r from-primary via-secondary to-primary"></div>
+        <div className="p-6 space-y-4">
+          <h3>{inspiration}</h3>
+          <p className="font-serif">{data.contribution}</p>
+          <button onClick={() => setOpen(true)} className="bg-secondary text-background font-medium font-heading px-4 py-2 cursor-pointer w-full">Edit</button>
+        </div>
       </div>
-    </div>
+      <Modal
+        heading="Edit Contribution"
+        open={open}
+        setOpen={setOpen}
+        elements={elements}
+        handleSubmit={handleSubmit}
+       />
+    </>
   );
 }
