e4e0e5ece7595394f696aba0faa4ed1ea4bd8f05
PresidentTree94 <karlysams1218@gmail.com>
2026-03-01 15:16:26 -0500

    Hid residence badges if they match homeland.

--- components/characterComps/Character.tsx
+++ components/characterComps/Character.tsx
@@ -17,7 +17,7 @@ export default function Character({ data }: { data: { character: CharacterType, 
         <div>
           <div className="flex items-center gap-2 flex-wrap font-semibold font-body text-xs uppercase tracking-tighter mb-2">
             {data.homeland && <span className="bg-background/80 text-primary px-2 py-1 backdrop-blur-sm">{data.homeland.name}, {data.homeland.kingdoms.name}</span>}
-            {data.residence && <span className="bg-secondary/80 text-[hsl(25,30%,15%)] px-2 py-1 backdrop-blur-sm">{data.residence.name}, {data.residence.kingdoms.name}</span>}
+            {data.residence && data.homeland.name !== data.residence.name && <span className="bg-secondary/80 text-[hsl(25,30%,15%)] px-2 py-1 backdrop-blur-sm">{data.residence.name}, {data.residence.kingdoms.name}</span>}
           </div>
           <h3 className="text-white text-2xl">{data.character.name}</h3>
           <p className="font-body text-secondary italic text-sm border-l-2 border-secondary pl-2">Inspired by {data.character.inspiration_id === null ?"???" : data.character.inspirations.name}</p>
